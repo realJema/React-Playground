1569e3ab8cd2a3c607054995cfc95b2862cc431a
realjema <realjema@gmail.com>
2020-04-18 23:16:24 +0100

    ads website converted to react components

--- src/Projects/ads/App.js
+++ src/Projects/ads/App.js
@@ -1,135 +1,44 @@
 import React from "react";
 import './App.css';
 
+// importing the cards 
+import SmallCard from './Components/layout/SmallCard';
+import LongCard from './Components/layout/LongCard';
+import SquareCard from "./Components/layout/SquareCard";
+import LongestCard from "./Components/layout/LongestCard";
+
 // {/* Hompage which contains the link to all the projects I am working on */}
 class Ads extends React.Component {
-    // constructor(props) {
-    //    super(props);
-         
-    //    this.state = {
-    //       data: ''
-    //    }
-    //    this.updateState = this.updateState.bind(this);
-    // };
-    // updateState (event){
-    //     this.setState({
-    //         data: event.target.value
-    //       });
-    // }
     render () {
 
         return (
             <div className="container-fluid col-md-8">
                 <div className="container-fluid row">
-                     <div className="card bg-dark text-white col-md-3">
-                        {/* <img src={require('./Components/images/card.PNG')} className="card-img" alt="..." /> */}
-                        <div className="card-img-overlay">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text">Last updated 3 mins ago</p>
-                        </div>
-                    </div>
-                     <div className="card bg-dark text-white col-md-6 middle-spacing">
-                        {/* <img src={require('./Components/images/card.PNG')} className="card-img" alt="..." /> */}
-                        <div className="card-img-overlay">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text">Last updated 3 mins ago</p>
-                        </div>
-                    </div>
-                     <div className="card bg-dark text-white col-md-3">
-                        {/* <img src={require('./Components/images/card.PNG')} className="card-img" alt="..." /> */}
-                        <div className="card-img-overlay">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text">Last updated 3 mins ago</p>
-                        </div>
-                    </div>
+                     <SmallCard />
+                     <LongCard />
+                     <SmallCard />
                 </div>
                 <div className="container-fluid row">
-                     <div className="card2 bg-dark text-white col-md-6">
-                        {/* <img src={require('./Components/images/card.PNG')} className="card-img" alt="..." /> */}
-                        <div className="card-img-overlay">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text">Last updated 3 mins ago</p>
-                        </div>
-                    </div>
+                    <SquareCard />
                     <div className="col-md-6 no-padding">
                         <div className="row">
-                            <div className="card bg-dark text-white col-md-6 right-spacing">
-                                {/* <img src={require('./Components/images/card.PNG')} className="card-img" alt="..." /> */}
-                                <div className="card-img-overlay">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p className="card-text">Last updated 3 mins ago</p>
-                                </div>
-                            </div>
-                            <div className="card bg-dark text-white col-md-6">
-                                {/* <img src={require('./Components/images/card.PNG')} className="card-img" alt="..." /> */}
-                                <div className="card-img-overlay">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p className="card-text">Last updated 3 mins ago</p>
-                                </div>
-                            </div>
+                            <LongCard />
+                            <LongCard />
                         </div>
                         <div className="row">
-                            <div className="card bg-dark text-white col-md-12">
-                                {/* <img src={require('./Components/images/card.PNG')} className="card-img" alt="..." /> */}
-                                <div className="card-img-overlay">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p className="card-text">Last updated 3 mins ago</p>
-                                </div>
-                            </div>
+                            <LongestCard />
                         </div>
                     </div>
                      
                 </div>
                 <div className="container-fluid row">
-                     <div className="card bg-dark text-white col-md-12">
-                        {/* <img src={require('./Components/images/card.PNG')} className="card-img" alt="..." /> */}
-                        <div className="card-img-overlay">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text">Last updated 3 mins ago</p>
-                        </div>
-                    </div>
+                    <LongestCard />
                 </div>
                 <div className="container-fluid row">
-                     <div className="card bg-dark text-white col-md-3 right-spacing-3">
-                        {/* <img src={require('./Components/images/card.PNG')} className="card-img" alt="..." /> */}
-                        <div className="card-img-overlay">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text">Last updated 3 mins ago</p>
-                        </div>
-                    </div>
-                     <div className="card bg-dark text-white col-md-3 right-spacing-3">
-                        {/* <img src={require('./Components/images/card.PNG')} className="card-img" alt="..." /> */}
-                        <div className="card-img-overlay">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text">Last updated 3 mins ago</p>
-                        </div>
-                    </div>
-                     <div className="card bg-dark text-white col-md-3 right-spacing-3">
-                        {/* <img src={require('./Components/images/card.PNG')} className="card-img" alt="..." /> */}
-                        <div className="card-img-overlay">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text">Last updated 3 mins ago</p>
-                        </div>
-                    </div>
-                     <div className="card bg-dark text-white col-md-3">
-                        {/* <img src={require('./Components/images/card.PNG')} className="card-img" alt="..." /> */}
-                        <div className="card-img-overlay">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text">Last updated 3 mins ago</p>
-                        </div>
-                    </div>
+                     <SmallCard />
+                     <SmallCard />
+                     <SmallCard />
+                     <SmallCard />
                 </div>
             </div>
         );
---
+++ src/Projects/ads/Components/layout/SquareCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// {/* Hompage which contains the link to all the projects I am working on */}
+class SquareCard extends React.Component {
+    render () {
+
+        return (
+            <div className="card2 bg-dark text-white col-md-6">
+                <div className="card-img-overlay">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text">Last updated 3 mins ago</p>
+                </div>
+            </div>
+        );
+    }
+}
+export default SquareCard;
