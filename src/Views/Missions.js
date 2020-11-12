bef2ae9cb988c713ffc804effe6251e25b3bc194
realjema <realjema@gmail.com>
2020-11-12 14:28:59 +0100

    link fix

--- src/Views/Missions.js
+++ src/Views/Missions.js
@@ -4,9 +4,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 // {/* Hompage which contains the link to all the projects I am working on */}
 export default function Missions() {
     return (
-        <div className="main_container">
-          <h6 className="brand" >NATIVE Missions & Vision</h6><br/>
-
-        </div>
-  );
+		<div className="main_container">
+			<h6 className="brand">NATIVE Missions & Vision</h6>
+			<br />
+		</div>
+	);
 }
